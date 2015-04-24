780c6f3bdea9df6cf6f3243b4d191c0f579294d1
encadyma <encadyma@gmail.com>
2015-04-23 19:34:42 -0700

    Set up gulp, included instructions.

--- README.md
+++ README.md
@@ -15,3 +15,9 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+###Building
+
+Clone, install `gulp`, run `npm install`.
+
+As a dependency for `gulp-scss-lint`, run `gem install scss-lint`.
--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,83 @@
 var gulp = require('gulp');
 
-gulp.task('default', function() {
-  // Stuff
+var gutil = require('gulp-util');
+
+var sasslint = require('gulp-scss-lint');
+var sass = require('gulp-sass');
+var minify_css = require('gulp-minify-css');
+
+var coffee = require('gulp-coffee');
+var uglify = require('gulp-uglify');
+
+gulp.task('sass', function() {
+  // Compiles src/scss to res/css
+  gulp.src('./src/scss/*.scss')
+    .pipe(sass({includePaths:['/src/scss/']}))
+    .pipe(gulp.dest('./res/css'));
 });
+
+gulp.task('scss-lint', function() {
+  gulp.src('./src/scss/*.scss')
+    .pipe(sasslint());
+});
+
+gulp.task('css-minify', function() {
+  return gulp.src('./res/css/*.css')
+    .pipe(minify_css({compatibility: 'ie8'}))
+    .pipe(gulp.dest('./res/css'));
+});
+
+gulp.task('coffee', function() {
+  // Compiles src/cs to res/js
+  gulp.src('./src/cs/*.coffee')
+    .pipe(coffee({bare: true}).on('error', gutil.log))
+    .pipe(gulp.dest('./res/js'));
+});
+
+gulp.task('uglify', function() {
+  return gulp.src('./res/js/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./res/js'));
+});
+
+gulp.task('move-images', function() {
+  return gulp.src('./src/images/*')
+    .pipe(gulp.dest('./res/images'));
+});
+
+gulp.task('move-textures', function() {
+  return gulp.src('./src/textures/*')
+    .pipe(gulp.dest('./res/textures'));
+});
+
+gulp.task('move-audio', function() {
+  return gulp.src('./src/audio/*')
+    .pipe(gulp.dest('./res/audio'));
+});
+
+gulp.task('default', ['scss-lint', 'sass', 'css-minify', 'coffee', 'uglify', 'move-images', 'move-textures', 'move-audio'], function() {
+  gulp.watch('./src/scss/*.scss', function() {
+    gulp.run('scss-lint', 'sass', 'css-minify');
+  });
+  
+  gulp.watch('./src/cs/*.coffee', function() {
+    gulp.run('coffee', 'uglify');
+  });
+  
+  gulp.watch('./src/images/*', function() {
+    gulp.run('move-images');
+  });
+  gulp.watch('./src/textures/*', function() {
+    gulp.run('move-textures');
+  });
+  gulp.watch('./src/audio/*', function() {
+    gulp.run('move-audio');
+  });
+});
+
+// Coffee logging
+
+var coffeeStream = coffee({bare: true});
+ 
+// Error listener for coffee
+coffeeStream.on('error', gutil.log);
